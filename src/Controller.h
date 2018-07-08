c35f5e8d4c7aa0c663ee11bbcc728d6ae7b65d4a
connorcarpenter <connorcarpenter@gmail.com>
2018-07-08 15:24:58 -0600

    got basic outline working

---
+++ src/Controller.h
@@ -0,0 +1,84 @@
+#pragma once
+
+//
+// Created by connor on 4/9/18.
+//
+
+#include "../../sova/src/Sova/Sova.h"
+#include "../../oryol/code/Modules/Core/String/String.h"
+#include "../../oryol/code/Modules/Core/Containers/Array.h"
+
+using namespace Sova;
+
+namespace DsprFrontend
+{
+    class Controller {
+    public:
+        Controller();
+        void onGameStart();
+        void onGameUpdate();
+
+        void onLoadProgress();
+        void onLoadFinish();
+
+        App app;
+        Viewport viewport;
+        Camera camera;
+        Container world;
+        Sprite nina;
+    };
+
+    Controller::Controller() { }
+
+    void Controller::onGameStart() {
+
+        app = App(640, 360, "Demo");
+
+        Array<String> resources;
+        resources.Add("images/myNinaSmall.png");
+        resources.Add("images/owl.png");
+
+        app.load(resources)
+           .onProgress(
+              [&]() {
+                 onLoadProgress();
+              })
+           .onFinish(
+              [&]() {
+                 onLoadFinish();
+              });
+
+        app.start();
+    }
+
+    void Controller::onLoadProgress() {
+        // load progress here
+    }
+
+    void Controller::onLoadFinish() {
+        // after loading is done
+        camera = Camera(app.width, app.height, world);
+        viewport = Viewport(0, 0, app.width, app.height, camera);
+        app.addViewport(viewport);
+
+        nina = Sprite(app, "images/myNinaSmall.png");
+
+        world.addChild(nina);
+
+        nina.position.set(82, 82);
+        nina.onUpdate(
+           [&]() {
+              nina.position.x += 1;
+           });
+
+        app.onUpdate(
+           [&]() {
+              onGameUpdate();
+           });
+    }
+
+    void Controller::onGameUpdate() {
+        // this is the game loop
+        world.updateChildren();
+    }
+}
---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by connor on 4/9/18.
+//
+
+#include "Controller.h"
+
+using namespace DsprFrontend;
+
+int main() {
+    DsprFrontend::Controller controller = DsprFrontend::Controller();
+    controller.onGameStart();
+    return 0;
+}
