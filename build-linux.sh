2a63df177319218e2486cd6adeeb10ff46542f17
connorcarpenter <connorcarpenter@gmail.com>
2018-07-09 13:29:30 -0600

    updated build scripts to put content folder in the right place

--- build-linux.sh
+++ build-linux.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 ./fips build linux-make-debug
+rm -rf ../fips-deploy/dspr-frontend/content
+cp -R content ../fips-deploy/dspr-frontend/content
--- build-wasm.sh
+++ build-wasm.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 ./fips build wasm-make-release
+rm -rf ../fips-deploy/dspr-frontend/wasm-make-release/content
+cp -R content ../fips-deploy/dspr-frontend/wasm-make-release/content
